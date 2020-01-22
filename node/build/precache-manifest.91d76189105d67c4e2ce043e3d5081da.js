70669b6a03b655906a538eae1e26d65bc0267f28
李武侯 <liwuhou@liwuhoudeMacBook-Air.local>
2020-01-22 21:18:47 +0800

    初次打包

---
+++ node/build/precache-manifest.91d76189105d67c4e2ce043e3d5081da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2445d9c636eb394cf881a9f5501bd527",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78e8bd9ab8ddbb322900",
+    "url": "/static/css/main.22de0aa1.chunk.css"
+  },
+  {
+    "revision": "07edc681808d093671c3",
+    "url": "/static/js/2.c8004768.chunk.js"
+  },
+  {
+    "revision": "78e8bd9ab8ddbb322900",
+    "url": "/static/js/main.4edce76c.chunk.js"
+  },
+  {
+    "revision": "0a00185933f836502722",
+    "url": "/static/js/runtime~main.1c32e248.js"
+  }
+]);
