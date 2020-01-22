5b38cd10e4e5ce0f4d614e643ea0914993d354be
李武侯 <liwuhou@liwuhoudeMacBook-Air.local>
2020-01-22 20:49:09 +0800

    更改react打包的最终目录

--- chat_react/config/paths.js
+++ chat_react/config/paths.js
@@ -69,7 +69,8 @@ const resolveModule = (resolveFn, filePath) => {
 module.exports = {
   dotenv: resolveApp('.env'),
   appPath: resolveApp('.'),
-  appBuild: resolveApp('build'),
+  // appBuild: resolveApp('build'),
+  appBuild: path.resolve(__dirname, '../../node/build'),
   appPublic: resolveApp('public'),
   appHtml: resolveApp('public/index.html'),
   appIndexJs: resolveModule(resolveApp, 'src/index'),
