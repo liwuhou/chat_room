ce89fba726162f86f7fc7ee3f2e81eeff802c87b
= <=>
2020-01-22 17:22:41 +0800

    删除两个无用文件

--- node/web/test.js
+++
@@ -1,9 +0,0 @@
-// import {readFile} from '../utils'
-const {readFile} = require('../utils');
-
-const data = readFile('../storage/chat_message_list.json');
-data.then((data) => {
-    const res = data.toString();
-    console.log('mkLog: res', JSON.parse(res));
-
-})
